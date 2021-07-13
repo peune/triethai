3411d7997c3224aa6db5b3ff57f990d4032c11b2
peune <peune.i3@gmail.com>
2021-07-13 14:42:01 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name='pytrie',
+setup(name='triethai',
       version='1.0',
       description='Trie for search, approximate search, and spell correction written in Python',
-      url='https://github.com/peune/pytrie.git',
+      url='https://github.com/peune/triethai.git',
       author='Sanparith Marukatat',
       author_email='sanparith.marukatat@nectec.or.th',
       license='MIT',
-      packages=['pytrie'])
+      packages=['triethai'])
