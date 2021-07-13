301b03454430aae996bb2e8e320c06a3e3e64e99
Peune <sanparith.marukatat@nectec.or.th>
2021-07-13 14:58:02 +0700

    name change to make PYPI package

--- pytrie/__init__.py
+++
@@ -1,6 +0,0 @@
-from pytrie import (load_dictionary,
-                    nn_search,
-                    approx_nn_search,
-                    word_graph_construction,
-                    best_path,
-                    spell_correction)
---
+++ triethai/__init__.py
@@ -0,0 +1,6 @@
+from triethai import (load_dictionary,
+                      nn_search,
+                      approx_nn_search,
+                      word_graph_construction,
+                      best_path,
+                      spell_correction)
