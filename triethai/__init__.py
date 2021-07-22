356e8e6e171fcdd439a9eb42c70cf2c774f0098e
peune <peune.i3@gmail.com>
2021-07-22 11:43:44 +0700

    Update __init__.py
    ...

--- triethai/__init__.py
+++ triethai/__init__.py
@@ -1,6 +1,6 @@
-from triethai import (load_dictionary,
-                      nn_search,
-                      approx_nn_search,
-                      word_graph_construction,
-                      best_path,
-                      spell_correction)
+from .triethai import (load_dictionary,
+                       nn_search,
+                       approx_nn_search,
+                       word_graph_construction,
+                       best_path,
+                       spell_correction)
